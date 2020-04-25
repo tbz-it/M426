7625181752cab5d8c89c1506d6e2e59b55dcd73e
Marcel <marcel.bernet@ch-open.ch>
2020-04-25 11:53:14 +0200

    Update install.sh

--- scripts/install.sh
+++ scripts/install.sh
@@ -2,30 +2,8 @@
 #!/bin/bash
 # 
 
-# Umgebungsvariablen wie IP VM
-export ADDR=$(ifconfig wg0 | grep inet | cut '-d ' -f 10)
-[ "${ADDR}" == "" ] && { export ADDR=$(hostname -I | cut -d ' ' -f 1); }
-
-# intro.html - von k8swebui.sh erstellt - ueberschreiben
-cat <<%EOF% | sudo tee /var/www/html/intro.html
-<!DOCTYPE html PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN" "http://www.w3.org/TR/html4/loose.dtd">
-<html>
-<head>
-<meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
-<link rel="shortcut icon" href="https://kubernetes.io/images/favicon.png">
-<meta charset="utf-8" content="">
-<meta http-equiv="X-UA-Compatible" content="IE=edge">
-<meta name="viewport" content="width=device-width, initial-scale=1">
-<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css"
-    integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
-<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap-theme.min.css"
-    integrity="sha384-rHyoN1iRsVXV4nD0JutlnGaslCJuC7uwjduW9SVrLvRYooPp2bWYgmgJQIXwl/Sp" crossorigin="anonymous">
-</head>
-<body>
-   $(markdown README.md | envsubst)
-</body>
-</html>
-%EOF%
+# K8s WebUI mit eigener README.md neu erzeugen
+/opt/lernmaas/services/k8swebui.sh README.md master
 
 # DevOps Umgebung
 kubectl apply -f ~/M426/
