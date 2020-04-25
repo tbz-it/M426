393bfac20afaae8ed471cdd28f5727952b2dde29
Marcel <marcel.bernet@ch-open.ch>
2020-04-25 13:20:56 +0200

    Update install.sh

--- scripts/install.sh
+++ scripts/install.sh
@@ -3,7 +3,7 @@
 # 
 
 # K8s WebUI mit eigener README.md neu erzeugen
-sudo bash -x /opt/lernmaas/services/k8swebui.sh README.md master
+sudo bash -x /opt/lernmaas/services/k8swebui.sh README.md minimal
 
 # DevOps Umgebung
 kubectl apply -f ~/M426/
